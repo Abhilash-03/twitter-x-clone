99467cf78bbfa21a531571bcd6f92505a7d0464f
Abhilash <abhilashjha335@gmail.com>
2024-06-13 18:10:10 +0530

    add mini-profile to the sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,15 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link'
 import { FaXTwitter } from 'react-icons/fa6';
-import { HiHome } from 'react-icons/hi';
+import { HiDotsHorizontal, HiHome } from 'react-icons/hi';
 
 const Sidebar = () => {
   const {data: session} = useSession();
+  console.log(session);
 
   return (
-    <div className='flex flex-col gap-4 p-3'>
+    <div className='flex flex-col justify-between h-screen p-3'>
+      <div className='flex flex-col gap-4'>
     <Link href='/'>
       <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200 ' />
     </Link>
@@ -30,7 +32,23 @@ const Sidebar = () => {
       </button>
       )
     }
- 
+    </div>
+    {
+      session && (
+        <div className='text-gray-700 shadow-xl rounded-xl bg-gray-200 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 hover:rounded-full transition-all duration-200'>
+        <img
+          src={session.user.image}
+          alt='user-img'
+          className='h-10 w-10 rounded-full lg:mr-2'
+        />
+        <div className='hidden lg:inline'>
+          <h4 className='font-bold'>{session.user.name}</h4>
+          <p className='text-gray-500'>@{session.user.username}</p>
+        </div>
+        <HiDotsHorizontal className='h-5 xl:ml-8 hidden xl:inline' />
+      </div>
+      )
+    }
   </div>
   )
 }

fb7cf90b210c97f3db59bb9e7eaa09624091f291
Abhilash <abhilashjha335@gmail.com>
2024-06-15 00:20:38 +0530

    add upload post functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,13 @@ import { app } from "@/firebase";
 import { useSession } from "next-auth/react";
 import { useEffect, useRef, useState } from "react";
 import { HiOutlinePhotograph } from "react-icons/hi";
-import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage'
+import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage';
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  serverTimestamp,
+} from 'firebase/firestore';
 
 const Input = () => {
     const { data: session } = useSession();
@@ -11,6 +17,9 @@ const Input = () => {
     const [imageFileURL, setImageFileURL] = useState(null);
     const [selectedFile, setSelectedFile] = useState(null);
     const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [text, setText] = useState(''); 
+    const [postLoading, setPostLoading] = useState(false);
+    const db = getFirestore(app);
 
     const addImageToPost = (e) => {
        const file = e.target.files[0];
@@ -33,6 +42,8 @@ const Input = () => {
       const fileName = new Date().getTime() + '-' + selectedFile.name;
       const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+ 
+
       uploadTask.on(
         'state_changed',
         (snapshot) => {
@@ -55,6 +66,24 @@ const Input = () => {
       );
     }
 
+    const handleSubmit = async () => {
+      setPostLoading(true);
+      const docRef = await addDoc(collection(db, 'posts'), {
+        uid: session.user.uid,
+        name: session.user.name,
+        username: session.user.username,
+        text,
+        profileImg: session.user.image,
+        timestamp: serverTimestamp(),
+        image: imageFileURL,
+      });
+      setPostLoading(false);
+      setText('');
+      setImageFileURL(null);
+      setSelectedFile(null);
+      location.reload();
+    };
+
     if(!session) return null;
   return (
     <div className='flex border-b border-gray-200 p-3 space-x-3 w-full'>
@@ -68,13 +97,14 @@ const Input = () => {
         className='w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700 '
         placeholder='Whats happening'
         rows='2'
-    
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       ></textarea>
       {selectedFile && (
           <img
             src={imageFileURL}
             alt='image'
-            className={`w-full max-h-[250px] object-cover cursor-pointer
+            className={`w-full max-h-[250px] object-cover cursor-pointer ${imageFileUploading ? 'animate-pulse' : ''}
            `}
           />
         )}
@@ -91,7 +121,9 @@ const Input = () => {
             onChange={addImageToPost}
           />
           <button
+          disabled={text.trim() === '' || postLoading || imageFileUploading}
             className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+            onClick={handleSubmit}
           >
             Post
           </button>
